6c3b0353a702ad625d94084a644628af3e894a14
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-07-27 12:19:04 +0530

    update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var ammount = document.querySelector("#bill-amount");
 var cash = document.querySelector("#cash");
 var btn = document.querySelector("#btn-chk");
+var outputvalue = document.querySelector("#output"); 
+var message = document.querySelector("#error-message");
 
 function countnotes(balance) {
     var notes= [2000,500,200,100,50,20,10,1];
@@ -8,22 +10,36 @@ function countnotes(balance) {
     var rem = balance;
     for(i in  notes){
         notesnumber = rem/notes[i];
-        
-        console.log(notes[i] +","+ Math.floor(notesnumber));
-        rem = balance%notes[i];
+        console.log(notes[i] +","+ Math.trunc(notesnumber));
+        rem = rem%notes[i];
+        console.log(rem);
     }
 }
 
 function check() {
     console.log(ammount.value);
     console.log(cash.value);
+    
+
+    if (ammount.value < 0){
+        console.log("invalid ammount");
+        showmessage("invalid ammount");
+    }
+
     if ((ammount.value) < (cash.value)){
         var balance = cash.value-ammount.value;
-        console.log("ammount to be raturned "+ balance);
+        console.log("ammount to be returned "+ balance);
+        showmessage("amount to be returned"+balance);
         countnotes(balance);
     }else{
         console.log("Invalid Transaction");
+        showmessage("Invalid aamount");
     }
+}
+
+function showmessage(msg){
+    message.style.display ="block";
+    message.innerText = msg;
 }
 
 btn.addEventListener("click",check);
