e9b5f6d9ac7d2f43fc5dd83e15c24601ac12fda1
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-07-27 17:59:31 +0530

    added next button and function

--- app.js
+++ app.js
@@ -1,45 +1,74 @@
-var ammount = document.querySelector("#bill-amount");
+var amount = document.querySelector("#bill-amount");
 var cash = document.querySelector("#cash");
 var btn = document.querySelector("#btn-chk");
 var outputvalue = document.querySelectorAll(".notesnumber") ; 
-var message = document.querySelector("#error-message");
+var message1 = document.querySelector("#error-message1");
+var message2 = document.querySelector("#error-message2");
+var notes= [2000,500,200,100,50,20,10,5,1];
+var btnCheck = document.querySelector("#check-amount");
+var afterchechk = document.querySelector(".aftercheck");
+var notesnumber;
 
 function countnotes(balance) {
-    var notes= [2000,500,200,100,50,20,10,1];
-    var notesnumber;
     var rem = balance;
     for(i in  notes){
         notesnumber = rem/notes[i];
         outputvalue[i].innerText = Math.trunc(notesnumber);
-        console.log(notes[i] +","+ Math.trunc(notesnumber));
-        rem = rem%notes[i];
-        
+        rem = rem%notes[i];    
+    }
+}
+
+function checkAmount(){
+    if (amount.value < 0){
+        showmessage1("invalid amount");
+    }else if (amount.value==0){
+        showmessage1("Bill amount zero nothing to cash");
+    }else {
+        deletemsg1();
+        afterchechk.style.display="block";
+        btnCheck.style.display="none";
     }
 }
 
 function check() {
-    deletemsg();
-    if (ammount.value <= 0){
-        console.log("invalid amount");
-        showmessage("invalid amount");
+    deletemsg2();
+    if (amount.value < 0){
+        showmessage2("Invalid Bill Amount");
         return ;
+    }else if (amount.value==0){
+        showmessage2("Bill amount zero")
     }
-    if ((ammount.value) <= (cash.value)){
-        var balance = cash.value-ammount.value;
-        
+    if ((amount.value) <= (cash.value)){
+        var balance = cash.value-amount.value;
         countnotes(balance);
     }else {
-        showmessage("Invalid amount need  more cash");
+        showmessage2("Invalid Amount!   Need more cash");
     }
 }
 
-function showmessage(msg){
-    message.style.display ="block";
-    message.innerText = msg;
+function showmessage1(msg){
+    message1.style.display ="block";
+    message1.innerText = msg;
 }
 
-function deletemsg(){
-    message.style.display="none";
+function showmessage2(msg){
+    message2.style.display ="block";
+    message2.innerText = msg;
 }
 
+function deletemsg1(){
+    message1.style.display="none";
+}
+
+function deletemsg2(){
+    message2.style.display="none";
+}
+
+function beforecheck(){
+    afterchechk.style.display="none";
+}
+
+beforecheck();
+
+btnCheck.addEventListener("click",checkAmount);
 btn.addEventListener("click",check);
