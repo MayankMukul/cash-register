401463da6d0630f17e8534f7d3d4dcef5e66f0d4
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-07-28 10:57:10 +0530

    error update

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var amount = document.querySelector("#bill-amount");
 var cash = document.querySelector("#cash");
 var btn = document.querySelector("#btn-chk");
 var outputvalue = document.querySelectorAll(".notesnumber") ; 
-var message1 = document.querySelector("#error-message1");
-var message2 = document.querySelector("#error-message2");
+var message = document.querySelector("#error-message");
+
 var notes= [2000,500,200,100,50,20,10,5,1];
 var btnCheck = document.querySelector("#check-amount");
 var afterchechk = document.querySelector(".aftercheck");
@@ -22,49 +22,40 @@ function countnotes(balance) {
 
 function checkAmount(){
     if (amount.value < 0){
-        showmessage1("*Enter Valid Amount");
+        showmessage("*Enter Valid Amount");
     }else if (amount.value==0){
-        showmessage1("*Bill Amount Zero Nothing To Cash");
+        showmessage("*Bill Amount Zero Nothing To Cash");
     }else {
-        deletemsg1();
+        deletemsg();
         afterchechk.style.display="block";
         btnCheck.style.display="none";
     }
 }
 
 function check() {
-    deletemsg2();
+    deletemsg();
     table.style.display="block";
     if (amount.value < 0){
-        showmessage2("*Enter Valid Bill Amount");
+        showmessage("*Enter Valid Bill Amount");
         return ;
     }else if (amount.value==0){
-        showmessage2("*Bill Amount Zero")
+        showmessage("*Bill Amount Zero")
     }
     if ((amount.value) <= (cash.value)){
         var balance = cash.value-amount.value;
         countnotes(balance);
     }else {
-        showmessage2("Invalid Amount!   Need More Cash");
+        showmessage("Invalid Amount!   Need More Cash");
     }
 }
 
-function showmessage1(msg){
-    message1.style.display ="block";
-    message1.innerText = msg;
+function showmessage(msg){
+    message.style.display ="block";
+    message.innerText = msg;
 }
 
-function showmessage2(msg){
-    message2.style.display ="block";
-    message2.innerText = msg;
-}
-
-function deletemsg1(){
-    message1.style.display="none";
-}
-
-function deletemsg2(){
-    message2.style.display="none";
+function deletemsg(){
+    message.style.display="none";
 }
 
 function beforecheck(){
